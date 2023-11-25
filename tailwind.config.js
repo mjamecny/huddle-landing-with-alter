977111c5875b0c834fd59911425bab18a7bb96fe
mjamecny <jamecny.m@tutanota.com>
2023-11-25 11:33:50 +0100

    add colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,12 @@ module.exports = {
       sans: ["'Open Sans'", "sans-serif"],
       poppins: ["Poppins", "sans-serif"],
     },
+    colors: {
+      pink: "hsl(322, 100%, 66%)",
+      veryPaleCyan: "hsl(193, 100%, 96%)",
+      veryDarkCyan: "hsl(192, 100%, 9%)",
+      grayishBlue: "hsl(208, 11%, 55%)",
+    },
   },
   plugins: [],
 }
